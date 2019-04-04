3a70b78fa5b53e8b3d701cc91d9abf1db9667dce
Lucas Pereira Lopes <lkogici@gmail.com>
2019-04-03 21:43:24 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Curso Angular
+Organização dos arquivos referente ao curso de Angular disponibilizado por Loiane Groner.
---
+++ hello-typescript/main.ts
@@ -0,0 +1,26 @@
+var minhaVAR = 'minha variavel';
+
+function minhaFunc(x, y){
+    return x + y;
+}
+
+// Transpiler = compilar ES 2015 para javaScript
+
+//ES6 ou ES 2015
+let num = 2;
+const PI  = 3.14;
+
+var numeros = [1, 2, 3];
+numeros.map(function(valor) {
+    return valor * 2;
+});
+numeros.map(valor => valor * 2); //ES 6
+
+class Matematica {
+    soma(x, y){
+        return x + y;
+    }
+}
+
+var n1: any = 'sdfsdf';
+n1 = 4;
